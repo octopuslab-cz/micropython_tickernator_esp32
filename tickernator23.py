9097e38448d6d1663fb07fb4da4670fbae872d4a
Honza S. Copak <bonum.comunae@volny.cz>
2023-07-19 12:20:14 +0200

    fix d8

--- tickernator23.py
+++ tickernator23.py
@@ -1,8 +1,8 @@
 from time import sleep
 from ticker import Ticker, bitcoin_usd
-from ticker import time_init, get_hh_mm_ss, disp7_pause, show_moving
-from utils.octopus_lib import getFree
-from components.display7 import Display7
+from ticker import time_init, get_hh_mm_ss, disp8_pause, show_moving
+from octopus_lib import getFree
+from components.display8 import Display8
 from utils.wifi_connect import WiFiConnect
 
 print("OctopusLAB - bitcoin tickernator")
@@ -14,19 +14,19 @@ t.print_config_info()
 spi, ss = t.spi_init()
 
 print("[Display init]")
-d7 = Display7(spi, ss) # 8 x 7segment display init
+d8 = Display8(spi, ss) # 8 x 7segment display init
 
-d7.write_to_buffer('octopus')
-d7.display()
+d8.write_to_buffer('octopus')
+d8.display()
 sleep(3)
-d7.intensity = 7
-disp7_pause(d7)
-d7.intensity = t.intensity # DISPLAY_INTENSITY
+d8.intensity = 7
+disp8_pause(d8)
+d8.intensity = t.intensity # DISPLAY_INTENSITY
 
 print("[WiFi connect]")
 net = WiFiConnect()
 net.connect()
-d7.show("WiFi")
+d8.show("WiFi")
 sleep(2)
 
 if not net.isconnected():
@@ -40,15 +40,15 @@ while True:
     getFree(True)
     btc = bitcoin_usd()
     print(btc)
-    #disp7_pause(d7)
-    show_moving(d7,str(int(btc)))
+    #disp8_pause(d8)
+    show_moving(d8,str(int(btc)))
     
     for i in range(3):
-        d7.show(str(int(btc)))
+        d8.show(str(int(btc)))
         sleep(DELAY_BTC)
-        d7.show("USD-BTC")
+        d8.show("USD-BTC")
         sleep(1.5)
     
         for t in range(10):
-            d7.show(get_hh_mm_ss(rtc))
+            d8.show(get_hh_mm_ss(rtc))
             sleep(1)
